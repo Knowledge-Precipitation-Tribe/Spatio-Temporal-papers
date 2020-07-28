a9ac434518d679c901beffbbc409083d5f1183a5
ubuntu <supeng842499467@gmail.com>
2020-07-28 22:06:11 +0800

    STGCN-pytorch

---
+++ models/STGCN/STGCN-pytorch/models/base_model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-#
+'''
+# Name:         base_model
+# Description:  
+# Author:       neu
+# Date:         2020/7/28
+'''
+
---
+++ models/STGCN/STGCN-pytorch/models/trainer.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-#
+'''
+# Name:         trainer
+# Description:  
+# Author:       neu
+# Date:         2020/7/28
+'''
+
